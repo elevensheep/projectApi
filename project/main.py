03c1a4f852f30d2512f0b46f4a472e8e494a545e
elevensheep <fttc100@gmail.com>
2025-03-21 14:15:24 +0900

    finish

--- project/main.py
+++ project/main.py
@@ -1,26 +1,27 @@
 from MySQLDatabase import MySQLDatabase
 from Nlp import Nlp
-from Crowling import Crowling  # Crowling 클래스가 있는 파일을 import 해야 합니다
+from Crowling import Crowling  
+import sys
 
-
-# 이 구문은 직접 실행될 때만 실행됩니다.
 if __name__ == "__main__":
+    
+    sys.stdout.reconfigure(encoding='utf-8')
     # 크롤러 인스턴스 생성
     crawler = Crowling()
-    
+    db = MySQLDatabase()
+    nlp = Nlp()
     # 크롤러 실행
     newsData = crawler.wordExtraction()
-    
-    nlp = Nlp()
     
     similar_news_data = nlp.get_similar_keywords(newsData)
     
     tokens = nlp.train_book_model_and_get_tokens()
     
-    db = MySQLDatabase()
+    db.truncateBooksKeyword()
     
-    # db.insert_books_keywords(isbn_tokens=tokens)
-    # db.insert_top_keywords(newsData=similar_news_data)
+    db.insert_books_keywords(isbn_tokens=tokens)
+    db.insert_top_keywords(newsData=similar_news_data)
+    
     
     fetchBookData = db.fetch_books_keywords()
     fetchNewsData = db.fetch_today_news()
