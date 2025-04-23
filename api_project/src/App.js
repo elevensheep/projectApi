d69acde602b3de0d9b6828cddd15e696f4e84e82
elevensheep <fttc100@gmail.com>
2025-04-23 12:17:24 +0900

    structure

--- api_project/src/App.js
+++ api_project/src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Component/Header'
+import MyPage from './MyPage/MyPage';
+import ListPage from './ListPage/ListPage';
+import LoginPage from './LoginPage/LoginPage';
+import MainPage from './MainPage/MainPage';
+import RecommendPage from './RecommendPage/RecommendPage';
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/list" element={<ListPage />} />
+          <Route path="/books" element={<LoginPage />} />
+          <Route path='/mypage' element={<MyPage />} />
+          <Route path='/Recommend' element={<RecommendPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
